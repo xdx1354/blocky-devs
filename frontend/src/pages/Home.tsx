d7b26126799f823a18f8b62827cb8d8f1deb8f89
xdx1354 <stanislaw1354@gmail.com>
2024-08-23 20:42:27 +0200

    basic UI added

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -8,15 +8,29 @@ import '../styles/tailwind.css';
 
 const Home: FC = () => {
 
-    const handleClick = () => {
+    const connectWallet = () => {
         console.log('clicked');
     }
 
     return (
-       <div>
-            <Button className="App-link" variant="destructive" onClick={handleClick}>DELETE</Button>
-       </div>
+        <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-600">
+            <header className="text-center mb-6">
+                <h1 className="text-4xl font-bold">Connect Wallet</h1>
+                <h3 className="text-2xl">MetaMask</h3>
+            </header>
+            <div>
+                <Button
+                    variant="destructive"
+                    onClick={connectWallet}
+                    className="text-lg py-3 px-6" // Add custom classes to enlarge the button
+                >
+                    Connect
+                </Button>
+            </div>
+        </div>
     );
-}
+};
+
+
 
 export default Home;

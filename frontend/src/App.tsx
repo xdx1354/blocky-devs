2081e2b3e17171f9946625afe078af801f3beac1
xdx1354 <stanislaw1354@gmail.com>
2024-08-24 00:02:45 +0200

    useWallet hook added, metamask wallet connected

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,33 +10,6 @@ function App() {
 
 
     return (
-        // <div className="App">
-        //     <header className="h-screen flex flex-col items-center justify-center">
-        //         <img
-        //             src={logo}
-        //             className="w-[40vmin] animate-[spin_10s_linear_infinite]"
-        //             alt="logo"
-        //         />
-        //         <p>
-        //             Edit <code>src/App.tsx</code> and save to reload.
-        //         </p>
-        //         <Button asChild variant="link">
-        //             <a
-        //                 href="https://reactjs.org"
-        //                 target="_blank"
-        //                 rel="noopener noreferrer"
-        //             >
-        //                 Learn React
-        //             </a>
-        //         </Button>
-        //         <Button
-        //             variant="destructive"
-        //             onClick={() => setCount((count) => count + 1)}
-        //         >
-        //             Count is {count}
-        //         </Button>
-        //     </header>
-        // </div>
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,43 +1,50 @@
 import { Button } from '../components/ui/button';
-import React, {FC} from 'react';
+import React, { FC, useState } from 'react';
 import '../styles/App.css';
 import '../styles/tailwind.css';
-import {useNavigate} from "react-router";
-
-
-
+import { useNavigate } from "react-router";
+import { Web3 } from "web3";
+import {undefined} from "zod";
+import useWallet from "../utils/useWallet";
 
 const Home: FC = () => {
-
     const navigate = useNavigate();
 
-    const connectWallet = () => {
-        console.log('clicked');
+    const {
+        connectedAccount,
+        error,
+        balance,
+        connectWallet,
+        isConnected
+    } = useWallet();
 
-        // HERE HANDLE CONNECTING TO WALLET
-
-        navigate('/dex');
-    }
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-600">
+        <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-600 text-white">
             <header className="text-center mb-6">
                 <h1 className="text-4xl font-bold">Connect Wallet</h1>
                 <h3 className="text-2xl">MetaMask</h3>
             </header>
             <div>
-                <Button
-                    variant="destructive"
-                    onClick={connectWallet}
-                    className="text-lg py-3 px-6" // Add custom classes to enlarge the button
-                >
-                    Connect
-                </Button>
+                {!isConnected && <Button
+                                                variant="destructive"
+                                                onClick={connectWallet}
+                                                className="text-lg py-3 px-6"
+                                            >
+                                                Connect
+                                            </Button>
+                }
+
+
+                {error && <p className="text-red-500 mt-4">{error}</p>}
+
+                <p>Connected Account: {connectedAccount}</p>
+                <p>Balance: {balance} ETH</p>
             </div>
+            { isConnected && <Button onClick={() => navigate('/dex')}> Go to DEX </Button>}
+
         </div>
     );
 };
-
-
 
 export default Home;

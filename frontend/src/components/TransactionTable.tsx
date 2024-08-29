60045a496edb596ca1bbbaa8881922d863a579ba
xdx1354 <stanislaw1354@gmail.com>
2024-08-29 12:06:31 +0200

    table with sorting, filtering and pagination added

---
+++ frontend/src/components/TransactionTable.tsx
@@ -0,0 +1,220 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "./ui/table";
+import {
+    ColumnDef,
+    SortingState,
+    flexRender,
+    getCoreRowModel,
+    getPaginationRowModel,
+    useReactTable,
+    PaginationState,
+} from "@tanstack/react-table";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { ArrowUpDown } from "lucide-react";
+import {TransactionTableData, PaginatedResponse} from "../types/types";
+
+export const columns: ColumnDef<TransactionTableData>[] = [
+    {
+        id: "select",
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+        accessorKey: "transactionSender",
+        header: "Transaction Sender",
+        cell: ({ row }) => (
+            <div className="capitalize">{row.getValue("transactionSender")}</div>
+        ),
+    },
+    {
+        accessorKey: "transactionDate",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Date
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        cell: ({ row }) => <div>{row.getValue("transactionDate")}</div>,
+    },
+    {
+        accessorKey: "exchangeRate",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Exchange Rate
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        cell: ({ row }) => <div>{row.getValue("exchangeRate")}</div>,
+    },
+    {
+        accessorKey: "erc20Amount",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                ERC20 Amount
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        cell: ({ row }) => <div>{row.getValue("erc20Amount")}</div>,
+    },
+    {
+        accessorKey: "ethAmount",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                ETH Amount
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        cell: ({ row }) => <div>{row.getValue("ethAmount")}</div>,
+    },
+];
+
+export function TransactionsTable() {
+    const [sorting, setSorting] = useState<SortingState>([]);
+    const [columnFilters, setColumnFilters] = useState<string>("");
+    const [data, setData] = useState<TransactionTableData[]>([]);
+    const [pagination, setPagination] = useState<PaginationState>({
+        pageIndex: 0,
+        pageSize: 10,
+    });
+    const [pageCount, setPageCount] = useState<number>(1);
+
+    const fetchData = async () => {
+        const sortBy = sorting.map(sort => sort.id).join(',');
+        const sortDirection = sorting.map(sort => sort.desc ? 'DESC' : 'ASC').join(',');
+
+        const response = await fetch(
+            `http://localhost:8080/v1/transactions?pageNumber=${pagination.pageIndex}&pageSize=${pagination.pageSize}&sortDirection=${sortDirection}&sortBy=${sortBy}&filterBy=${columnFilters}`
+        );
+        const result: PaginatedResponse = await response.json();
+        setData(result.content);
+        setPageCount(result.totalPages);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [sorting, columnFilters, pagination.pageIndex, pagination.pageSize]);
+
+
+    const table = useReactTable({
+        data,
+        columns,
+        onSortingChange: setSorting,
+        onPaginationChange: setPagination,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        state: {
+            sorting,
+            pagination
+        },
+        manualPagination: true,
+        manualFiltering: true,
+        manualSorting: true,
+        pageCount: pageCount
+    });
+
+    return (
+        <div className="w-full">
+            <div className="flex items-center py-4">
+                <Input
+                    placeholder="Filter senders addresses..."
+                    value={columnFilters}
+                    onChange={(event) => {
+                        setColumnFilters(event.target.value.trim());
+                    }}
+                    className="max-w-sm"
+                />
+            </div>
+            <div className="rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )}
+                                    </TableHead>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    key={row.id}
+                                    data-state={row.getIsSelected() && "selected"}
+                                >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell
+                                    colSpan={columns.length}
+                                    className="h-24 text-center"
+                                >
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+            <div className="flex items-center justify-between py-4">
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.previousPage()}
+                    disabled={!table.getCanPreviousPage()}
+                >
+                    Previous
+                </Button>
+                <span>
+                    Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+                </span>
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.nextPage()}
+                    disabled={!table.getCanNextPage()}
+                >
+                    Next
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default TransactionsTable;
